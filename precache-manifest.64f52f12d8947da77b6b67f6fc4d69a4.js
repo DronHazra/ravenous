7b1d65f7c5a2c48c0658c4f5270ad3d4e791935c
Dron <dron@intellear.com>
2020-04-29 12:11:25 -0400

    Updates

---
+++ precache-manifest.64f52f12d8947da77b6b67f6fc4d69a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f21cde9ff23b427fcd883ceb004eee",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "606e80fe75a8709ddeeb",
+    "url": "/ravenous/static/css/main.18bd43f2.chunk.css"
+  },
+  {
+    "revision": "e69340e1dc0a2346b517",
+    "url": "/ravenous/static/js/2.bc84ee82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.bc84ee82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606e80fe75a8709ddeeb",
+    "url": "/ravenous/static/js/main.4afa9db3.chunk.js"
+  },
+  {
+    "revision": "a7aa978227b7a1ea93c4",
+    "url": "/ravenous/static/js/runtime-main.61552fde.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
